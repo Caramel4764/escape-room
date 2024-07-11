e7b02423054f5fc6171386affa9b5aea67c44cd2
Caramel4764 <nina.dong4764@gmail.com>
2024-07-10 18:58:02 -0700

    doc: add hat hanger and carpet. All image created in dom by js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let woodFloorDiv = document.querySelector('#wood-floor-div')
+let setting = document.querySelector('#room');
 let wallDiv = document.querySelector('#wall-div');
 let floor = document.createElement('img');
 let wall = document.createElement('img')
-
+let divider = document.createElement('img');
 let rooms = {
   "dims": {
     x: 1000,
@@ -11,7 +12,30 @@ let rooms = {
   "entrance": {
     'wall': './assets/wall/wood-wall.jpg',
     "floor": './assets/floor/wood-floor.png',
+    'divider': "./assets/floor/floor-divider.png",
     'entities': [
+      {
+        "name": 'hatHanger',
+        'src': './assets/furniture/hat-stand.png',
+        "desc": "It's a hat hanger",
+        'dims': {
+          'x': "600px",
+          'y': "140px",
+          "width": "400px",
+          'z': "3",
+        }
+      },
+      {
+        "name": 'welcomeMat',
+        'src': './assets/floor/carpet.png',
+        "desc": "It's a hat hanger",
+        'dims': {
+          'x': "303px",
+          'y': "480px",
+          "width": "400px",
+          'z': "3",
+        }
+      },
       {
         "name": 'exitDoor',
         'src': './assets/wall/exit-door.png',
@@ -23,35 +47,31 @@ let rooms = {
           'z': 3,
         }
       },
-      {
-        "name": 'exitDoor',
-        'src': './assets/wall/exit-door.png',
-        "desc": "The door which you came in",
-        'dims': {
-          'x': "100px",
-          'y': "82px",
-          "width": "200px",
-          'z': 3,
-        }
-      }
     ]
   },
 }
 function newRoom(room) {
   floor.src = currRoom.floor;
+  floor.style.y='200px';
   floor.setAttribute('id', 'wood-floor');
   woodFloorDiv.appendChild(floor);
   wall.src = currRoom.wall;
   wallDiv.append(wall);
+  divider.src = currRoom.divider;
+  divider.style.zIndex = '10';
+  woodFloorDiv.append(divider);
   currRoom.entities.map((entity)=>{
     let entityImg = document.createElement("img");
     entityImg.src=entity.src;
     entityImg.style.top=entity.dims.y;
     entityImg.style.left=entity.dims.x;
+    entityImg.style.width=entity.dims.width;
+    entityImg.style.zIndex=entity.dims.z;
+    setting.appendChild(entityImg);
   })
 }
 
-let currRoom = rooms.entrance;
+let currRoom = rooms.entrance
 newRoom(currRoom);
 
 
