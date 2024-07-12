a837d9038cc4b82e47b240594c95d6ed2aa5696b
Caramel4764 <nina.dong4764@gmail.com>
2024-07-12 16:49:01 -0700

    add displayRoom function to its own file.

---
+++ javascript/functions/displayRoom.js
@@ -0,0 +1,41 @@
+import {inspectMenuInfo} from '../data/inspectInfoMenu.js'
+import {displayInspect} from './displayInspectMenu.js';
+import {toggleInspectMenu} from './toggleInspectMenu.js';
+
+const floor = document.createElement('img');
+const wall = document.createElement('img')
+const divider = document.createElement('img');
+const woodFloorDiv = document.querySelector('#wood-floor-div')
+const setting = document.querySelector('#room');
+const wallDiv = document.querySelector('#wall-div');
+const inspectText = document.querySelector('#inspect-text');
+const inspectImg = document.querySelector('#inspect-image');
+
+function displayRoom(currRoom) {
+  floor.src = currRoom.floor;
+  floor.style.y='200px';
+  floor.setAttribute('id', 'wood-floor');
+  woodFloorDiv.appendChild(floor);
+  wall.src = currRoom.wall;
+  wallDiv.append(wall);
+  divider.src = currRoom.divider;
+  divider.style.zIndex = '10';
+  woodFloorDiv.append(divider);
+  currRoom.entities.map((entity)=>{
+    let entityImg = document.createElement("img");
+    entityImg.src=entity.src;
+    entityImg.style.top=entity.dims.y;
+    entityImg.style.left=entity.dims.x;
+    entityImg.style.width=entity.dims.width;
+    entityImg.style.zIndex=entity.dims.z;
+    entityImg.addEventListener('click', () => {
+      displayInspect(entity.desc, 100);
+      inspectText.innerHTML= inspectMenuInfo.chunkedText[0];
+      inspectImg.src=entity.src;
+      toggleInspectMenu();
+    })
+    setting.appendChild(entityImg);
+  })
+}
+
+export {displayRoom}
