447c7e90e3bc0c8303102b0258ffd95d3a1b01b7
Caramel4764 <nina.dong4764@gmail.com>
2024-07-12 16:51:26 -0700

    doc: delete now unused variable and hide inspect menu div when game starts.

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 import {rooms} from './javascript/data/rooms.js';
-import {inspectMenuInfo} from './javascript/data/inspectInfoMenu.js';
-import {toggleInspectMenu} from './javascript/functions/toggleInspectMenu.js';
-import {displayInspect} from './javascript/functions/displayInspectMenu.js';
 import {displayRoom} from './javascript/functions/displayRoom.js';
-
-
-
 
 let currRoom = rooms.entrance;
 displayRoom(currRoom);
