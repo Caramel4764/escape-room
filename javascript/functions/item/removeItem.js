ce33e79baed4dc4b6403b870c07aebc1d1b5e945
caramel4764 <nina.dong4764@gmail.com>
2024-07-21 11:17:01 -0700

    WIP: set up code to easily test removeItem

--- javascript/functions/item/removeItem.js
+++ javascript/functions/item/removeItem.js
@@ -3,6 +3,7 @@ import { resetSelectedItem } from "./resetSelectedItem.js";
 let inventory = document.querySelector('#inventory');
 
 function removeItem (entity) {
+  console.log(entity)
   for (let i = 0; i < player.inventory.length; i++) {
     if (player.inventory[i].name == entity.puzzle.itemNeeded) {
       player.inventory.splice(i, 1);
--- javascript/functions/map/toggleMap.js
+++ javascript/functions/map/toggleMap.js
@@ -1,11 +1,14 @@
 let map = document.querySelector('#map-div');
 import {addItem} from '../item/addItem.js';
+import { removeItem } from '../item/removeItem.js';
+
 function toggleMap () {
   if (map.style.visibility == 'visible') {
     map.style.visibility = 'hidden';
     //testing
-    /*addItem('flashLightItem')
-    addItem('battery')*/
+    addItem('flashLightItem')
+    addItem('battery')
+    removeItem('battery');
   } else {
     map.style.visibility = 'visible';
   }
