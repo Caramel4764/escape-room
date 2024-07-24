950e6e13ae99f595af6a7b14c8e6346ce3d10e5d
caramel4764 <nina.dong4764@gmail.com>
2024-07-23 18:14:19 -0700

    doc: add text below desktop icon. Adjusting desktop icon to look nicer.

--- javascript/functions/createDesktopIcon.js
+++ javascript/functions/createDesktopIcon.js
@@ -1,20 +1,41 @@
 
 function createDesktopIcon(desktop, type, x, y) {
+  let desktopIconDiv = document.createElement('div');
+  desktopIconDiv.style.display='flex';
+  desktopIconDiv.style.position='absolute';
+  desktopIconDiv.style.flexDirection = 'column';
+  desktopIconDiv.style.justifyContent = 'center';
+  desktopIconDiv.style.alignItems = 'center';
+  desktopIconDiv.style.left = x * 70 + 'px';
+  desktopIconDiv.style.top = y * 70 + 'px';
+  desktopIconDiv.classList.add('desktop-icon');
+
+
   let desktopIcon = document.createElement('img');
   if (type == 'file') {
     desktopIcon.src='./assets/misc/computer/file.png';
   } else if (type == 'folder') {
     desktopIcon.src='./assets/misc/computer/folder.png';
   }
-  desktopIcon.classList.add('desktop-icon');
-  desktopIcon.style.left = x * 70 + 'px';
-  desktopIcon.style.top = y * 70 + 'px';
-  desktopIcon.addEventListener('mouseenter', function() {
-    desktopIcon.style.backgroundColor = 'green'
+  desktopIcon.classList.add('desktop-icon-img');
+
+  desktopIcon.style.position = 'relative';
+  desktopIcon.style.pointerEvents = 'none';
+  desktopIconDiv.addEventListener('mouseenter', function() {
+    desktopIconDiv.style.backgroundColor = 'green'
   })
-  desktopIcon.addEventListener('mouseout', function() {
-    desktopIcon.style.backgroundColor = 'black'
+  desktopIconDiv.addEventListener('mouseout', function() {
+    desktopIconDiv.style.backgroundColor = 'black'
   })
-  desktop.appendChild(desktopIcon);
+  
+  desktopIconDiv.appendChild(desktopIcon);
+  let desktopIconText = document.createElement('p');
+  desktopIconText.textContent = 'test.txt';
+  //desktopIconText.style.margin = '2px';
+  desktopIconText.style.fontSize = '12px';
+  desktopIconText.style.pointerEvents = 'none';
+  desktopIconDiv.appendChild(desktopIconText);
+
+  desktop.appendChild(desktopIconDiv);
 }
 export {createDesktopIcon}
