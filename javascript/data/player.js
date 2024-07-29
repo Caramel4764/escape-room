a79deae13e9128c176078692d0b6afd058fb8f38
caramel4764 <nina.dong4764@gmail.com>
2024-07-28 23:53:40 -0700

    add input to clock puzzle.

--- javascript/data/player.js
+++ javascript/data/player.js
@@ -1,7 +1,14 @@
+let date = new Date();
+
 let player = {
   "selectedItem": 'none',
   "selectedItemDom" : 'none',
   "currRoom": "not initalized",
   "inventory": [],
+  'time': {
+    'hour': date.getHours(),
+    'minute': date.getMinutes(),
+    'second': date.getSeconds(),
+  }
 }
 export {player}
--- javascript/functions/mountClock.js
+++ javascript/functions/mountClock.js
@@ -1,27 +1,59 @@
 import { createPopup } from "./createPopup.js";
+import { player } from "../data/player.js";
+function limitFunctionInput(limitInfo) {
+  let {value, min, max, change} = limitInfo;
+  if (change < 0) {
+    if (value<=min) {
+      value=max;
+    } else {
+      value--;
+    }
+    console.log('subract')
+  }
+  if (change > 0) {
+    if (value>=max) {
+      value=min;
+    } else {
+      value++;
+    }
+    console.log('add')
+  }
+  return value;
+}
 
 let clockDiv = document.createElement("div");
-
-function createTimeInput() {
+let timeInputFullTimeDiv = document.createElement("div");
+timeInputFullTimeDiv.setAttribute("id","timeInputFullTimeDiv");
+function createTimeInput(timeUnit) {
   let timeInputDiv = document.createElement("div");
   timeInputDiv.classList.add("time-input-div");
   let hourInput = document.createElement("div");
   hourInput.classList.add("time-input");
-  hourInput.style.position="relative";
   hourInput.style.display="flex";
   timeInputDiv.appendChild(hourInput);
-
   let hourUp = document.createElement("button");
   hourUp.textContent = "+";
   hourInput.appendChild(hourUp);
   let hourCounter = document.createElement("p");
-  hourCounter.textContent = "12";
+  hourCounter.textContent = player.time[timeUnit];
   hourCounter.classList.add("time-input-counter");
   hourInput.appendChild(hourCounter);
   let hourDown = document.createElement("button");
   hourDown.textContent = "-";
+  hourDown.addEventListener("click", function () {
+    if (timeUnit=='hour') {
+      player.time.hour = limitFunctionInput({'value':player.time[timeUnit], 'min':0, 'max':23, 'change':-1});
+    }
+    hourCounter.textContent=player.time[timeUnit]
+  })
+  hourUp.addEventListener("click", function () {
+    if (timeUnit=='hour') {
+      player.time.hour = limitFunctionInput({'value':player.time[timeUnit], 'min':0, 'max':23, 'change':1});
+    }
+    hourCounter.textContent=player.time[timeUnit]
+  })
   hourInput.appendChild(hourDown);
-  return timeInputDiv;
+  timeInputFullTimeDiv.appendChild(timeInputDiv);
 }
 
 function mountClock() {
@@ -31,15 +63,12 @@ function mountClock() {
   clockImg.src = "./assets/furniture/fireplace/grandfather-clock.png";
   clockDiv.appendChild(clockImg);
   menuDiv.appendChild(clockImg);
-  
-  clockDiv.appendChild(createTimeInput());
-  clockDiv.appendChild(createTimeInput());
-  clockDiv.appendChild(createTimeInput());
+  createTimeInput('hour');
+  createTimeInput('minute');
+  createTimeInput('second');
 
 
-
-
-
+  clockDiv.appendChild(timeInputFullTimeDiv);
   menuDiv.appendChild(clockDiv);
 }
 
