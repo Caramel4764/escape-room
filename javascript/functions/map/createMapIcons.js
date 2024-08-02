aaa52918d997bd7a9f0ad3c651ea1d613d83dae3
caramel4764 <nina.dong4764@gmail.com>
2024-08-02 14:34:47 -0700

    fix | feat: connect dream area to real area. With the goggles equipped, the game icons change and bring player to new area. Map fully disappears and fixed all icons going to bedroom bug.

--- javascript/functions/map/createMapIcons.js
+++ javascript/functions/map/createMapIcons.js
@@ -1,50 +1,47 @@
 import {locations} from '../../data/locations.js'
 import {rooms} from '../../data/rooms.js'
 import { goRoom } from "../room/goRoom.js";
-import {toggleMap} from "./toggleMap.js";
+import { handleMapToggle } from './handleMapToggle.js';
+let map = document.querySelector('#map-div');
+let dreamMap = document.querySelector("#dream-map");
+let dreamMapIcon = document.querySelector("#dream-map-icon-div");
 let flashlight = document.querySelector('#flashlight');
 
-//this function creates the setting
 function createMapIcons() {
-  let mapIconDiv;
-  let mapIcon;
-  let currRoom;
   locations.map((location)=>{
+    let mapIconDiv = document.createElement('div');
+    mapIconDiv.setAttribute('class', 'map-icon-div');
+    let mapIcon = document.createElement('img');
+    mapIcon.style.visibility = 'inherit';
+    mapIconDiv.style.visibility = 'inherit';
+    mapIcon.setAttribute('id', location.id);
+    mapIcon.setAttribute('class', 'map-icon-div-img');
+    mapIcon.style.zIndex=100;
     for (let i = 1; i < Object.keys(rooms).length; i++) {
       if (location.id == Object.keys(rooms)[i]) {
-          currRoom = rooms[Object.keys(rooms)[i]];
-              console.log(currRoom)
-
-        //if (!currRoom.isDream) {
-          mapIconDiv = document.createElement('div');
-          mapIconDiv.setAttribute('class', 'map-icon-div');
-          mapIcon = document.createElement('img');
-          mapIcon.setAttribute('id', location.id);
-          mapIcon.setAttribute('class', 'map-icon-div-img');
-          mapIcon.style.zIndex=100;
-          mapIconDiv.style.left = currRoom.icon.x;
-          mapIcon.src = currRoom.icon.src;
-          mapIconDiv.style.top = currRoom.icon.y;
-          mapIconDiv.addEventListener('click', ()=>{
-            goRoom(currRoom.name);
-            if (currRoom.isDark) {                flashlight.style.visibility = 'visible';
-            } else {
+        let currRoom = rooms[Object.keys(rooms)[i]];
+        mapIconDiv.style.left = currRoom.icon.x;
+        mapIcon.src = currRoom.icon.src;
+        mapIconDiv.style.top = currRoom.icon.y;
+        mapIconDiv.addEventListener('click', ()=>{
+          goRoom(currRoom.name);
+          if (currRoom.isDark) {
+            flashlight.style.visibility = 'visible';
+          } else {
             flashlight.style.visibility = 'hidden';
-            }
-            toggleMap();
-          })
-        //}
+          }
+          handleMapToggle();
+          mapIcon.style.visibility = 'inherit';
+          mapIconDiv.style.visibility = 'inherit';
+        })
+        if (currRoom.isDream) {
+          document.querySelector('#dream-map-icon-div').appendChild(mapIconDiv);
+        } else {
+          document.querySelector('#map-icon-div').appendChild(mapIconDiv);
+        }
+        mapIconDiv.appendChild(mapIcon);
       }
     }
-    //console.log(currRoom)
-    //console.log(currRoom.isDream)
-    if (currRoom.isDream) {
-      document.querySelector('#dream-map-icon-div').appendChild(mapIconDiv);
-      console.log('here')
-    } else {
-      document.querySelector('#map-icon-div').appendChild(mapIconDiv);
-    }
-    mapIconDiv.appendChild(mapIcon);
   })
 }
 export {createMapIcons}
--- script.js
+++ script.js
@@ -8,26 +8,12 @@ import { mountFlashlight } from "./javascript/functions/popup/mount/mountFlashli
 import { mountComputer } from "./javascript/functions/popup/mount/mountComputer.js";
 import { mountClock } from "./javascript/functions/popup/mount/mountClock.js";
 import { createDreamMapIcons } from "./javascript/functions/map/createDreamMapIcons.js";
-import { toggle } from "./javascript/functions/toggle.js";
 import { addItem } from "./javascript/functions/item/addItem.js";
+import { handleMapToggle } from "./javascript/functions/map/handleMapToggle.js";
 
-let map = document.querySelector('#map-div');
-let dreamMap = document.querySelector("#dream-map");
-let dreamMapIcon = document.querySelector("#dream-map-icon-div");
-map.style.visibility = 'hidden';
-dreamMap.style.visibility = 'hidden';
 
 mapIcon.addEventListener("click", function () {
-  if (player.isDreamWorld) {
-    toggle(dreamMap);
-    toggle(dreamMapIcon)
-    toggle(map, false);
-  } else {
-    toggle(map);
-    toggle(dreamMap, false);
-    toggle(dreamMapIcon, false);
-
-  }
+  handleMapToggle();
 });
 
 for (let i = 1; i < Object.keys(rooms).length; i++) {
@@ -35,9 +21,9 @@ for (let i = 1; i < Object.keys(rooms).length; i++) {
   createRoomElement(player.currRoom);
 }
 
-goRoom("attic");
+goRoom("entrance");
 createMapIcons();
 mountFlashlight();
 mountComputer();
 mountClock();
-addItem('blueLightBulb');
+//addItem('blueLightBulb');
